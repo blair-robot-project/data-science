c4ae8ebeb707a4e2c940833b9ad95413df6b333f
Margaret <stickerstarlove@gmail.com>
2026-06-28 10:26:13 -0400

    fixed the nasty time (help why was it like negative a billion)

--- Shiny_organized/app.R
+++ Shiny_organized/app.R
@@ -1,8 +1,17 @@
 library(shiny)
 library(bslib)
+library(googlesheets4)
 
 data_sheet <- read_sheet("https://docs.google.com/spreadsheets/d/1hXbsPjKuyHZYW3LuDfqaSVnZSOkykmPnVyEZ2izvgvA/edit?usp=sharing")
 
+#functions
+time_fixer <- function(a_number) {
+  a_string <- as.character(a_number)
+  new_string <- substring(a_string, 11)
+  return(new_string)
+}
+
+#ui
 ui <- fluidPage(
   titlePanel("Attendance"),
   mainPanel(
@@ -10,9 +19,11 @@ ui <- fluidPage(
   )
 )
 
+#server
 server <- function(input, output) {
   output$attendanceTable <- renderTable({
-    data_sheet
+    fixed_data <- data_sheet |>
+      mutate(Time = time_fixer(Time))
   })
 }
 
--- Shiny_organized/process.R
+++ Shiny_organized/process.R
@@ -1,5 +1,18 @@
 #lets do this
+library(googlesheets4)
+library(dplyr)
 
 data_sheet <- read_sheet("https://docs.google.com/spreadsheets/d/1hXbsPjKuyHZYW3LuDfqaSVnZSOkykmPnVyEZ2izvgvA/edit?usp=sharing")
 
+time_fixer <- function(a_number) {
+#So like basically before I added this function it had a very wrong date 
+#included, so i got rid of it so that now its just the time 
+  
+  a_string <- as.character(a_number)
+  new_string <- substring(a_string, 11)
+  return(new_string)
+}
+  
 
+fixed_data <- data_sheet |>
+  mutate(Time = time_fixer(Time))
