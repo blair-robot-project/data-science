72d5498514340d0dac78d21893299a8ded779a85
Margaret <stickerstarlove@gmail.com>
2026-06-28 17:59:55 -0400

    biscuit is cute

--- Shiny_organized/process.R
+++ Shiny_organized/process.R
@@ -5,14 +5,18 @@ library(dplyr)
 data_sheet <- read_sheet("https://docs.google.com/spreadsheets/d/1hXbsPjKuyHZYW3LuDfqaSVnZSOkykmPnVyEZ2izvgvA/edit?usp=sharing")
 
 time_fixer <- function(a_number) {
-#So like basically before I added this function it had a very wrong date 
-#included, so i got rid of it so that now its just the time 
-  
-  a_string <- as.character(a_number)
-  new_string <- substring(a_string, 11)
-  return(new_string)
+    a_string <- as.character(a_number)
+    new_string <- substring(a_string, 11)
+    return(new_string)
 }
-  
 
 fixed_data <- data_sheet |>
-  mutate(Time = time_fixer(Time))
+    mutate(Time = time_fixer(Time))
+
+new_data <- fixed_data |>
+    group_by(`Student ID`, Date) |>
+    summarize(TimeSpent = as.character(max(as.numeric(Time)) - min(as.numeric(Time))),
+              TimeArrived = min(Time),
+              TimeLeft = max(Time),
+              `Student ID` = min(`Student ID`),
+              Date = min(Date))
