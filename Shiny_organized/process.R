276ef100a9ccd3fb008cffe2d9821fe3d843ef62
Margaret <stickerstarlove@gmail.com>
2026-07-17 17:11:27 -0400

    two unworking tabs?

--- Shiny_organized/process.R
+++ Shiny_organized/process.R
@@ -8,27 +8,56 @@ data_sheet <- read_sheet("https://docs.google.com/spreadsheets/d/1hXbsPjKuyHZYW3
 
 time_fixer <- function(a_number) {
     a_string <- as.character(a_number)
-    new_string <- substring(a_string, 12)
+    new_string <- substring(a_string, 11)
     return(new_string)
 }
-
 name_fixer <- function(id) {
     x <- as.character(id)
     string1 <- ifelse(grepl(" ", x), paste0(stringr::word(x, 1), stringr::word(x, 2)), x)
     return(string1)
 }
 
-fixed_data <- data_sheet |>
-    mutate(Time = time_fixer(Time),
-           StudentID = name_fixer(StudentID))
 
-new_data <- fixed_data |>
+# time_fixer <- function(a_number) {
+#     a_string <- as.character(a_number)
+#     new_string <- substring(a_string, 12)
+#     return(new_string)
+# }
+# 
+# name_fixer <- function(id) {
+#     x <- as.character(id)
+#     string1 <- ifelse(grepl(" ", x), paste0(stringr::word(x, 1), stringr::word(x, 2)), x)
+#     return(string1)
+# }
+# 
+# fixed_data <- data_sheet |>
+#     mutate(Time = time_fixer(Time),
+#            StudentID = name_fixer(StudentID))
+# 
+# new_data <- fixed_data |>
+#     group_by(StudentID, Date) |>
+#     reframe(TimeSpent = as.numeric(difftime(
+#             max(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE), 
+#             min(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE), 
+#             units = "mins"), digits = 1),
+#             TimeArrived = time_fixer(min(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE)),
+#             TimeLeft = time_fixer(max(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE)))
+
+
+new_data <- data_sheet |>
+    mutate(Time = time_fixer(Time),
+           StudentID = name_fixer(StudentID),
+           Date = as.POSIXct(Date, format="%Y:%m:%d")) |>
     group_by(StudentID, Date) |>
     reframe(TimeSpent = as.numeric(difftime(
-            max(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE), 
-            min(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE), 
-            units = "mins"), digits = 1),
-            TimeArrived = time_fixer(min(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE)),
-            TimeLeft = time_fixer(max(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE)))
+        max(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE), 
+        min(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE), 
+        units = "mins"), digits = 1),
+        TimeArrived = time_fixer(min(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE)),
+        TimeLeft = time_fixer(max(as.POSIXct(Time, format="%H:%M:%S"), na.rm = TRUE))) |>
+    arrange(Date)
 
+summary_data <- new_data |>
+    group_by(StudentID) |>
+    summarize(Total = floor(sum(TimeSpent)))
 
