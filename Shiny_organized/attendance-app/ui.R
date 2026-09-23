3c091b9eb4332aaba0c42519c8bb46b7644565d5
Margaret <stickerstarlove@gmail.com>
2026-09-23 18:36:58 -0400

    please ignore all of this

---
+++ Shiny_organized/attendance-app/ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(bslib)
+library(googlesheets4)
+library(dplyr)
+
+#ui
+ui <- fluidPage(
+    theme = bs_theme(
+        version = 5, 
+        bootswatch = "yeti",
+        bg = "#FFE9F9", 
+        fg = "#6E6D6D",
+        primary = "#E683A9",
+        secondary = "#F0B4C4"
+    ),
+    titlePanel("Attendance"),
+    tabsetPanel(
+        tabPanel("Raw data", tableOutput("attendanceTable")),
+        tabPanel("Summary", tableOutput("summaryTable")),
+        tabPanel(
+            title = "Graphs",
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput(
+                        inputId = "selected_id", 
+                        label = "Choose a Student:", 
+                        choices = unique(new_data$StudentID))
+                ),
+            mainPanel(plotOutput("individual_graph"))
+            )
+        )
+    ))
